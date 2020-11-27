0ff34175afc306b6139162e60fa884a4b6c1272a
Dilum Aluthge <dilum@aluthge.com>
2020-11-26 20:10:28 -0500

    Add some dependencies (#5)

--- src/FHIRClientXML.jl
+++ src/FHIRClientXML.jl
@@ -1,5 +1,9 @@
 module FHIRClientXML
 
+import Base64
+import HTTP
+import XMLDict
+
 f(x) = x + x
 
 end # end module FHIRClientXML
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using FHIRClientXML
 using Test
 
+import HTTP
+import XMLDict
+
 @testset "FHIRClientXML.jl" begin
     @test FHIRClientXML.f(1) == 2
 end
