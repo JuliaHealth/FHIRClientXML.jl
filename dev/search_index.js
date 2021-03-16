028d84c4f1a32a9b0589517e036795ce6bd547e8
Documenter.jl <documenter@juliadocs.github.io>
2021-03-16 11:58:03 +0000

    build based on 525a5be

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FHIRClientXML","category":"page"},{"location":"#FHIRClientXML","page":"Home","title":"FHIRClientXML","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FHIRClientXML]","category":"page"}]
+}
